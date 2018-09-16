ce98ddf0f3da2c9794be5f2f2059a1554f4e4358
Patryk <sfazzy@gmail.com>
2018-09-17 00:22:03 +0200

    rev 1

--- app/src/main/java/com/geophysics_consulting/myfirstdicee/MainActivity.java
+++ app/src/main/java/com/geophysics_consulting/myfirstdicee/MainActivity.java
@@ -2,6 +2,12 @@ package com.geophysics_consulting.myfirstdicee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollingButton;
+        rollingButton = findViewById(R.id.roll_button);
+
+        final ImageView leftdice = findViewById(R.id.image_leftDice);
+        final ImageView rightdice = findViewById(R.id.image_rightDice);
+
+        final int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+        rollingButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d("Dicee", "Ze button has been pressed");
+            Random randomNumberGenerator = new Random();
+
+            int number = randomNumberGenerator.nextInt(6);
+
+            Log.d("Dicee", "The random number is:" + number);
+
+            leftdice.setImageResource(diceArray[number]);
+
+            number = randomNumberGenerator.nextInt(6);
+
+            rightdice.setImageResource(diceArray[number]);
+        }
+    });
     }
 }
